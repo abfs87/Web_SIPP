0445071f7c6f77e7753b7495fd0ad18ae8cc691c
abfs87 <abfs87.nd@gmail.com>
2015-05-27 05:43:56 +0700

    versi 0.0

---
+++ pblc/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="css/styl.css">
+
+    <script src="js/jquery-2.1.4.min.js" type="text/javascript"></script>
+    <script src="js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="js/scrp.js" type="text/javascript"></script>
+</head>
+
+<body>
+    <div class="menu">
+        <div class="menu-bar navbar navbar-fixed-top">
+            <div class="container">
+                <div class="header">
+                    <!-- <div class="col-xs-8 col-md-6"> -->
+                    <nav class="list-menu">
+                        <ul class="nav nav-pills pull-right">
+                            <li><a href="" title="">TENTANG</a>
+                            </li>
+                            <li><a href="" title="">GALERI</a>
+                            </li>
+                            <li><a href="" title="">JADWAL</a>
+                            </li>
+                            <li><a href="" title="">KONTAK</a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <!-- </div> -->
+                    <p class="logoin">LOGO</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="cover">
+        <img src="img/cover.jpg" alt="">
+    </div>
+
+    <div class="content">
+
+    </div>
+</body>
+
+</html>
