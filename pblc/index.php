00b40db41991b123eeb784e35023c9c90c8f4e21
abfs87 <abfs87.nd@gmail.com>
2015-05-27 20:54:26 +0700

    profile phase

--- pblc/index.php
+++ pblc/index.php
@@ -20,32 +20,42 @@
         <div class="menu-bar navbar navbar-fixed-top">
             <div class="container">
                 <div class="header">
-                    <!-- <div class="col-xs-8 col-md-6"> -->
-                    <nav class="list-menu">
-                        <ul class="nav nav-pills pull-right">
-                            <li><a href="" title="">TENTANG</a>
-                            </li>
-                            <li><a href="" title="">GALERI</a>
-                            </li>
-                            <li><a href="" title="">JADWAL</a>
-                            </li>
-                            <li><a href="" title="">KONTAK</a>
-                            </li>
-                        </ul>
-                    </nav>
-                    <!-- </div> -->
-                    <p class="logoin">LOGO</p>
+                    <div class="col-md-6 forlogo">
+                        <div class="logoin">
+                        
+                    </div>
+                    
+                    </div>
+                    <div class="col-md-6 formenu"> 
+                        <nav class="list-menu">
+                            <ul class="nav nav-pills pull-right">
+                                <li><a href="" title="">TENTANG</a>
+                                </li>
+                                <li><a href="" title="">GALERI</a>
+                                </li>
+                                <li><a href="" title="">JADWAL</a>
+                                </li>
+                                <li><a href="" title="">KONTAK</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div> 
+                    
                 </div>
             </div>
         </div>
     </div>
 
-    <div class="cover">
-        <img src="img/cover.jpg" alt="">
-    </div>
-
     <div class="content">
+        <div class="container">
+            <div class="sampul">
 
+            </div>
+            <div class="profil">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, tenetur perferendis, minus ipsa consectetur quidem laboriosam et corrupti, placeat quia velit amet provident nesciunt impedit explicabo expedita illo! Sapiente, laboriosam.</p>
+            </div>
+        </div>
+        
     </div>
 </body>
 
